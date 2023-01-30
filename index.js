66b5e0dbeb949b7901b399019f6e4c7c607473f5
Anders Brownworth <anders-git@evantide.com>
2023-01-29 22:40:06 -0500

    make module work

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-const { Queue } = require('./lib/');
-
-const timeout = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
+module.exports = {
+    Queue: require('./lib')
 };
-
-const queue = new Queue();
-
-const i = setInterval(() => console.log(queue.length()), 250);
-
-console.log('queue 1');
-queue.enqueue(async () => {
-    console.log('start 1');
-    await timeout(1000);
-    console.log('end 1');
-});
-
-console.log('queue 2');
-queue.enqueue(async () => {
-    console.log('start 2');
-    await timeout(1000);
-    console.log('end 2');
-});
-
-setTimeout(() => {
-    console.log('queue 3');
-    queue.enqueue(async () => {
-	console.log('start 3');
-	await timeout(1000);
-	console.log('end 3');
-	clearInterval(i);
-    }, 1100);
-});
